e3d517a40942657d302de7e9ca5bd3e78f4bfdcb
Mariosam2 <marco.mariosaa@gmail.com>
2025-05-21 10:46:33 +0200

    trying to force websocket connection

--- src/helpers/socket.ts
+++ src/helpers/socket.ts
@@ -1,19 +1,22 @@
 import io from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_BASE_URL, {
-  port: "3000",
-  secure: true,
-  autoConnect: false,
-  reconnection: true,
-  reconnectionAttempts: 5,
-  reconnectionDelay: 500,
-  reconnectionDelayMax: 1000,
-  transportOptions: {
-    polling: {
-      withCredentials: true,
+export const socket = io(
+  "wss://chatappbackend-production-3fab.up.railway.app/",
+  {
+    secure: true,
+    autoConnect: false,
+    reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 500,
+    reconnectionDelayMax: 1000,
+    transports: ["websocket"],
+    transportOptions: {
+      polling: {
+        withCredentials: true,
+      },
+      websocket: {
+        withCredentials: true,
+      },
     },
-    websocket: {
-      withCredentials: true,
-    },
-  },
-});
+  }
+);
